29af261c934a1f7ca7548981568b665ace4cb7eb
Ramla-I <ijazramla@gmail.com>
2019-03-22 19:46:06 -0500

    testbench for throughput and latency

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,41 @@ extern crate rand;
 
 use num_complex::Complex32;
 use rand::prelude::*;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 mod matrixmult;
-
 use matrixmult::gemm;
 
+static MULT_DONE: AtomicUsize = AtomicUsize::new(0);
+
 fn main() {
-    let m = 64;
-    let num_iter = 1_000;
 
-    // create input matrix A and initialize with random numbers
-    let mut a: Vec<Complex32> = Vec::with_capacity(m*m);
-    let mut rng = rand::thread_rng();
+    /*** Part 1: Latency - how fast can you make matrix multiplication ***/
+    let num_iter = 1000;
 
-    for _ in 0..m*m {
-        a.push(Complex32{re: rng.gen(), im: rng.gen()});
-    }
-
-    // create input matrix B and initialize with random numbers
-    let mut b: Vec<Complex32> = Vec::with_capacity(m*m);
-    for i in 0..m*m {
-        b.push(Complex32{re: rng.gen(), im: rng.gen()});
-    }
-
-    // create output matrix C and clear
-    let mut c: Vec<Complex32> = Vec::with_capacity(m*m);
-
-    for _ in 0..m*m {
-        c.push(Complex32{re: 0.0, im: 0.0})
-    }
-
-    // now run matrix mult in a loop and note time
     let start = SystemTime::now();
     for _ in 0..num_iter {
-        let _ = gemm(&mut a, m, m, &mut b, m, m, &mut c);
+        run_matrixmult();
     }
     let duration = start.elapsed();
 
-    println!("time = {:?}", duration.unwrap());
+    // note down average latency
+    // latency = duration / num_iter
+
+
+
+    /*** Part 2: Throughput - how many matrix multiplications can you perform in parallel ***/
+
+    // Num of seconds to run the evaulation
+    let time = 30;
+
+    // we sleep for 30 seconds
+    sleep(Duration::new(30, 0));
+
+    // Note down values of measurement thread and total throughput
+    // throughput = MULT_DONE / time
 }
 
 fn print_matrix(mat: &Vec<Complex32>, rows: usize, cols: usize) {
@@ -52,4 +48,38 @@ fn print_matrix(mat: &Vec<Complex32>, rows: usize, cols: usize) {
         println!("");
     }
     println!("");
+}
+
+// generates a randomly intialized matrix in row major order
+fn generate_random_matrix(size: usize) -> Vec<Complex32> {
+    let mut a: Vec<Complex32> = Vec::with_capacity(size * size);
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..size*size {
+        a.push(Complex32{re: rng.gen(), im: rng.gen()});
+    }
+
+    a
+}
+
+// generates a zeroed matrix in row major order
+fn generate_zeroed_matrix(size: usize) -> Vec<Complex32> {
+    let mut a: Vec<Complex32> = Vec::with_capacity(size * size);
+
+    for _ in 0..size*size {
+        a.push(Complex32{re: 0.0, im: 0.0});
+    }
+    
+    a
+}
+
+fn run_matrixmult() {
+    let m = 64;
+    let a = generate_random_matrix(m);
+    let b = generate_random_matrix(m);
+    let mut c = generate_zeroed_matrix(m);
+
+    let _ = gemm(&a, m, m, &b, m, m, &mut c);
+
+    MULT_DONE.fetch_add(1, Ordering::Relaxed);
 }
